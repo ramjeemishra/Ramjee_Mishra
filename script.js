279358529ae83a12955d7608a39398bfc6ff8640
Ram <131847922+Rambhai567@users.noreply.github.com>
2023-09-06 20:17:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -31,3 +31,15 @@ setInterval(()=>{
 
 
       
+
+
+
+////code///
+fetch('code/cl.txt')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('text-content').textContent = data;
+    })
+    .catch(error => {
+      console.error('Error fetching the file:', error);
+    });
