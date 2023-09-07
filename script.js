4d3aa39e9b8db358014d3097c11f898ebed4cbd8
Ram <131847922+Rambhai567@users.noreply.github.com>
2023-09-07 22:09:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -43,3 +43,38 @@ fetch('code/cl.txt')
     .catch(error => {
       console.error('Error fetching the file:', error);
     });
+
+
+
+
+
+    ////copy button//
+    document.addEventListener("DOMContentLoaded", function () {
+    const copyButtons = document.querySelectorAll(".copy-button");
+
+    copyButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            const targetTextId = button.getAttribute("data-target");
+            const textContent = document.getElementById(targetTextId);
+            const iframe = button.previousElementSibling; // Get the previous sibling (iframe)
+
+            const iframeContent = iframe.contentDocument.body.textContent;
+
+            const tempInput = document.createElement("textarea");
+            tempInput.value = iframeContent;
+            document.body.appendChild(tempInput);
+
+            tempInput.select();
+            tempInput.setSelectionRange(0, 99999); // For mobile devices
+
+            document.execCommand("copy");
+
+            document.body.removeChild(tempInput);
+
+            button.textContent = "Copied!";
+            setTimeout(() => {
+                button.textContent = "Copy";
+            }, 2000);
+        });
+    });
+});
