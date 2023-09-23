2f7c40b71ca4a962af4b61ff8711ed00b505ab47
Ram <131847922+Rambhai567@users.noreply.github.com>
2023-09-23 20:39:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -78,3 +78,49 @@ fetch('code/cl.txt')
         });
     });
 });
+
+
+
+
+
+
+
+
+
+//progress scrollbar//
+
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.onscroll = function(){
+    let progressHeight = (window.pageYOffset / totalHeight) * 100; // Fix the = to *
+    progress.style.height = progressHeight + "%";
+}
+
+
+
+
+
+
+
+
+
+
+//progressbar//
+
+let options = {
+    startAngle: -1.55,
+    size: 150,
+    value: 0.85,
+    fill: {gradient: ['#a445b2', '#fa4299']}
+  }
+  $(".circle .bar").circleProgress(options).on('circle-animation-progress',
+  function(event, progress, stepValue){
+    $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+  });
+  $(".js .bar").circleProgress({
+    value: 0.70
+  });
+  $(".react .bar").circleProgress({
+    value: 0.60
+  });
